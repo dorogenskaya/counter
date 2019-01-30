eb6621b66562fad50d2b21db8d96c342ed932c3c
doraby <doraby1@gmail.com>
2019-01-30 09:05:52 +0300

    commit from webstorm

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,6 @@ import React, {Component} from 'react';
 class Counter extends Component {
     state = {
         count: 3,
-        // imageUrl: 'https://picsum.photos/200'
         tags: ['tag1','tag2','tag3']
     };
 
@@ -12,32 +11,34 @@ class Counter extends Component {
         if (this.state.tags.length === 0) return <p>"there are no tags"</p>;
         return <ul>{this.state.tags.map(tag => <li key ={tag} >{tag}</li>)}</ul>;
 
-    }
+    };
 
     formatCount(){
         const {count} = this.state;
         return count === 0 ? 'Zero' : count;
-    }
+    };
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
         return classes;
-    }
+    };
+
+    handleIncrement = () => {
+        this.setState ({count: this.state.count + 1})
+    };
 
     render() {
         return (
         <React.Fragment>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
+            {/*<button onClick={ () => {this.handleIncrement(product)}} className='btn btn-secondary btn-sm'>Increment</button>*/}
+            <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+
             {this.renderTags()}
         </React.Fragment>
         );
     }
-
-
 }
-
-// Counter.propTypes = {};
 
 export default Counter;
