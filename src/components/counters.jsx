ef12d30a6c187808b427527c19a4b4d1f98bc08c
doraby <doraby1@gmail.com>
2019-02-02 11:57:34 +0300

    commit from webstorm

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,15 +9,46 @@ export default class Counters extends Component {
             {id: 2, value:4},
             {id: 3, value:4},
             {id: 4, value:4 }
-        ]
-    }
+        ],
+        tags:  ['tag1','tag2','tag3']
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.filter(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters })
+    };
+
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({ counters })
+    };
+
+    handleDelete = counterId =>{
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters});
+    };
 
     render() {
         return (
             <div>
-                {this.state.counters.map(counter => <Counter key={counter.id} value={counter.value}>
-                </Counter>)}
-
+                <button
+                    onClick={this.handleReset}
+                    className='btn btn-secondary btn-sm m-2'>Reset</button>
+                {this.state.counters.map(counter =>
+                    <Counter
+                        key={counter.id}
+                        onDelete={this.handleDelete }
+                        onIncrement={this.handleIncrement}
+                        counter={counter}
+                        tags={this.state.tags}
+                        />
+                )}
             </div>
         );
     }
