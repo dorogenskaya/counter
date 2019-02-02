973a9693f640959211b3dce64acf22b8e2e4d603
doraby <doraby1@gmail.com>
2019-02-02 11:58:32 +0300

    commit from webstorm

---
+++ src/components/navBar.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="http://localhost:3000/">Navbar</a>
+            </nav>
+        );
+    }
+}
